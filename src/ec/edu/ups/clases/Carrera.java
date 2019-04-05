82006dd09cc966995ff823b330d51ece1796a0a6
jonnathan <jonnathan@LAPTOP-U2SAQOEF>
2019-04-04 21:41:15 -0500

    inicializar proyecto

--- src/ec/edu/ups/clases/Carrera.java
+++ src/ec/edu/ups/clases/Carrera.java
@@ -12,10 +12,74 @@ import java.util.List;
  * @author jonnathan
  */
 public class Carrera {
+
+    @Override
+    public String toString() {
+        return "Carrera{" + "codigo=" + codigo + ", nombre=" + nombre + ", materias=" + materias + ", numeroSemestres=" + numeroSemestres + ", numeroEstudiantes=" + numeroEstudiantes + ", titulo=" + titulo + ", materia=" + materia + '}';
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMaterias() {
+        return materias;
+    }
+
+    public void setMaterias(String materias) {
+        this.materias = materias;
+    }
+
+    public int getNumeroSemestres() {
+        return numeroSemestres;
+    }
+
+    public void setNumeroSemestres(int numeroSemestres) {
+        this.numeroSemestres = numeroSemestres;
+    }
+
+    public int getNumeroEstudiantes() {
+        return numeroEstudiantes;
+    }
+
+    public void setNumeroEstudiantes(int numeroEstudiantes) {
+        this.numeroEstudiantes = numeroEstudiantes;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public List<Materia> getMateria() {
+        return materia;
+    }
+
+    public void setMateria(List<Materia> materia) {
+        this.materia = materia;
+    }
     protected int codigo;
     private String nombre;
-    private String direccion;
-    private List<Carrera> carreras ;
+    private String materias;
+    private int numeroSemestres;
+    private int numeroEstudiantes;
+    private String titulo;
+    private List<Materia> materia ;
 
     
 }
