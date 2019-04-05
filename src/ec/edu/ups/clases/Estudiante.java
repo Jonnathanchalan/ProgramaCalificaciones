448d71d5dd7d5b209e6cd0c2acc5cf72589c62f8
jonnathan <jonnathan@LAPTOP-U2SAQOEF>
2019-04-04 21:06:00 -0500

    inicializar proyecto

--- src/ec/edu/ups/clases/Estudiante.java
+++ src/ec/edu/ups/clases/Estudiante.java
@@ -12,9 +12,28 @@ import java.util.List;
  * @author jonnathan
  */
 public class Estudiante {
+
+    @Override
+    public String toString() {
+        return "Estudiante{" + "codigo=" + codigo + ", carreras=" + carreras + '}';
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public List<Carrera> getCarreras() {
+        return carreras;
+    }
+
+    public void setCarreras(List<Carrera> carreras) {
+        this.carreras = carreras;
+    }
     protected int codigo;
-    private String nombre;
-    private String direccion;
     private List<Carrera> carreras ;
 
     
